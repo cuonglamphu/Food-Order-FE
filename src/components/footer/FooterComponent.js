6ead0cfedccf58ffef434b15dd83adb08b270c19
Lâm Phú Cường <Lpcuong8888@gmail.com>
2024-11-08 20:26:55 +0700

    chore: initialize project

---
+++ src/components/footer/FooterComponent.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./FooterComponent.css";
+
+const FooterComponent = () => {
+    return (
+        <footer className="footer-container">
+            <div className="footer-content">
+                <div className="pixel-logo">FOOD ORDER</div>
+
+                <div className="pixel-social">
+                    <a
+                        href="https://www.facebook.com/phucuong25.0/"
+                        className="pixel-icon"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    ></a>
+                    <a
+                        href="https://www.facebook.com/phucpham1803"
+                        className="pixel-icon"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    ></a>
+                    <a
+                        href="https://www.facebook.com/baolong.pham.7921975"
+                        className="pixel-icon"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    ></a>
+                </div>
+            </div>
+
+            <div className="pixel-copyright">
+                © 2024 CUONGDAY.COM. ALL RIGHTS RESERVED
+            </div>
+        </footer>
+    );
+};
+
+export default FooterComponent;
